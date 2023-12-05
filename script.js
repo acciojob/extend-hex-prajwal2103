42c5d486a43931d10373ca34cc6b1fef3188639b
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2023-12-05 18:31:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  let ans="";
+  for (let index = 0; index < shortHex.length; index++) {
+	  if(index==0){
+		  continue
+	  }
+  	ans+=shortHex.charAt(index)+shortHex.charAt(index)
+  }	
+	return ans;
 };
 
 // Do not change the code below.
